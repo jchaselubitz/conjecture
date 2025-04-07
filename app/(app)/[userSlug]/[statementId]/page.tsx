05916474a688d4242650611e2e9abfd68cbbbc4c
jchaselubitz <jchaselubitz@gmail.com>
2025-04-07 13:21:08 +0200

    linting

--- app/(app)/[userSlug]/[statementId]/page.tsx
+++ app/(app)/[userSlug]/[statementId]/page.tsx
@@ -1,12 +1,24 @@
 import { StatementContainer } from "@/containers/StatementContainer";
 import { getDraftsByStatementId } from "@/lib/actions/statementActions";
+import { createClient } from "@/supabase/server";
 export default async function CreatePage({
   params,
+  searchParams,
 }: {
   params: Promise<{ statementId: string }>;
+  searchParams: Promise<{ edit: string }>;
 }) {
-  const { statementId } = await params;
-  const drafts = await getDraftsByStatementId(statementId);
+  const supabase = await createClient();
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
 
-  return <StatementContainer drafts={drafts} />;
+  const userId = user?.id;
+  const { statementId } = await params;
+  const { edit } = await searchParams;
+  const drafts = await getDraftsByStatementId(statementId);
+  const isCreator = drafts[0].creatorId === userId;
+  const editMode = edit === "true" && isCreator;
+
+  return <StatementContainer edit={editMode} drafts={drafts} />;
 }
