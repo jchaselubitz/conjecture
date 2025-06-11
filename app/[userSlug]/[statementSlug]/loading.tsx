9f1e2ac8baeccb6865e0ab905b745eff721922db
jchaselubitz <jchaselubitz@gmail.com>
2025-06-11 18:05:49 +0200

    fixed statement suspense

--- app/[userSlug]/[statementSlug]/loading.tsx
+++ app/[userSlug]/[statementSlug]/loading.tsx
@@ -1,11 +1,10 @@
-'use client';
 import { Skeleton } from '@/components/ui/skeleton';
 
 export default function StatementLoading() {
   return (
     <div>
-      <div className="flex justify-center py-8 px-4 md:px-0">
-        <div className="flex flex-col h-full max-w-screen md:max-w-3xl">
+      <div className=" py-8 px-4 md:px-0 w-full">
+        <div className="mx-auto flex flex-col  md:max-w-3xl">
           <Skeleton className="w-full h-96" />
           <Skeleton className="w-full h-24" />
           <div className="flex flex-col gap-2">
--- app/[userSlug]/loading.tsx
+++ app/[userSlug]/loading.tsx
@@ -7,21 +7,19 @@ import { Skeleton } from '@/components/ui/skeleton';
 export default function UserLoading() {
   const segment = useSelectedLayoutSegment();
 
+  if (segment) return null;
+
   return (
     <div>
       <main className="flex-1 mx-auto bg-background container py-8 px-4 md:px-0">
-        {segment === null && (
-          <>
-            <div className="flex items-center justify-between pb-8 ">
-              <h1 className="text-3xl font-bold">Statements</h1>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-              {[1, 2, 3].map(statement => (
-                <Skeleton className="animate-pulse h-48 bg-gray-200 rounded-lg" key={statement} />
-              ))}
-            </div>
-          </>
-        )}
+        <div className="flex items-center justify-between pb-8 ">
+          <h1 className="text-3xl font-bold">Statements</h1>
+        </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {[1, 2, 3].map(statement => (
+            <Skeleton className="animate-pulse h-48 bg-gray-200 rounded-lg" key={statement} />
+          ))}
+        </div>
       </main>
     </div>
   );
