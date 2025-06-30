a62062f1dc0628735109640957776d4bcd1dc3e5
jchaselubitz <jchaselubitz@gmail.com>
2025-06-30 12:48:14 +0200

    more performance improvements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,21 +1,21 @@
-import typography from '@tailwindcss/typography';
-import type { Config } from 'tailwindcss';
+import typography from "@tailwindcss/typography";
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './containers/**/*.{js,ts,jsx,tsx,mdx}',
-    './contexts/**/*.{js,ts,jsx,tsx,mdx}'
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./containers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./contexts/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)'
-      }
-    }
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
-  plugins: [typography]
+  plugins: [typography],
 };
 export default config;
