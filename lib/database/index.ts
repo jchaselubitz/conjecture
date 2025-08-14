f6eb6c87ac9c1a880836d789b8f99d0682d0862a
jchaselubitz <jchaselubitz@gmail.com>
2025-08-14 15:35:10 +0200

    need to redeploy

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -27,6 +27,7 @@ const connectionString = process.env.POSTGRES_URL || '';
 
 // "write-supabase-ca": "node -e \"const fs=require('fs'); if(!process.env.SUPABASE_CA_PEM){console.error('SUPABASE_CA_PEM missing'); process.exit(1)} fs.writeFileSync('./supabase-ca.pem', process.env.SUPABASE_CA_PEM)\"",
 // "generate-deploy": "if [ -n \"$VERCEL\" ]; then NODE_OPTIONS=\"--dns-result-order=ipv4first\" yarn write-supabase-ca && PGSSLMODE=verify-full PGSSLROOTCERT=./supabase-ca.pem kysely-codegen; else kysely-codegen; fi",
+//
 
 const ssl = (() => {
   if (process.env.NODE_ENV === 'development' && !process.env.VERCEL) {
