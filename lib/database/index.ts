24aff66bd39d2787d2f29f511d3ee6ebb3b2422c
jchaselubitz <jchaselubitz@gmail.com>
2025-10-20 12:04:26 +0200

    are the timeout settings necessary?

--- lib/database/index.ts
+++ lib/database/index.ts
@@ -54,7 +54,9 @@ const db = new Kysely<DB>({
       connectionString: connectionString,
       // ssl: { rejectUnauthorized: true },
       max: 10,
-      min: 2
+      min: 2,
+      idleTimeoutMillis: 30000, // 30 seconds
+      connectionTimeoutMillis: 2000 // 2 seconds
     })
   })
 });
